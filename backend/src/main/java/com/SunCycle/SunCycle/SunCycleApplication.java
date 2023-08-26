014c8d55b7d36d8489f5b015bb7cc199cbfb959a
Eric Li <jili5381@uni.sydney.edu.au>
2023-08-26 12:39:36 +1000

    Backend Init Commit

---
+++ backend/src/main/java/com/SunCycle/SunCycle/SunCycleApplication.java
@@ -0,0 +1,13 @@
+package com.SunCycle.SunCycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunCycleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunCycleApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/SunCycle/SunCycle/SunCycleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SunCycle.SunCycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunCycleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
