12fcab4615689d47bb3d60c225492d646a1b4e32
edward <wzhe8830@uni.sydney.edu.au>
2023-08-26 15:28:31 +1000

    added UserController

--- backend/src/main/java/com/SunCycle/SunCycle/Repository/UserRepository.java
+++ backend/src/main/java/com/SunCycle/SunCycle/Repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.SunCycle.SunCycle.Repository;
 
 import com.SunCycle.SunCycle.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends JpaRepository<Integer, User> {
+public interface UserRepository extends CrudRepository<User, Integer> {
 }
