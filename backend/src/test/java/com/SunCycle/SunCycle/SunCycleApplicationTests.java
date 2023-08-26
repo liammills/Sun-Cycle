f750d134f1b1a08f82b48287ec3707b8cdaffe9a
edward <wzhe8830@uni.sydney.edu.au>
2023-08-26 14:20:50 +1000

    added mysql connector dependency

--- backend/src/test/java/com/SunCycle/SunCycle/SunCycleApplicationTests.java
+++ backend/src/test/java/com/SunCycle/SunCycle/SunCycleApplicationTests.java
@@ -2,12 +2,14 @@ package com.SunCycle.SunCycle;
 
 import org.junit.jupiter.api.Disabled;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Disabled;
 
 @SpringBootTest
 class SunCycleApplicationTests {
 
 	@Disabled
 	void contextLoads() {
+
 	}
 
 }
