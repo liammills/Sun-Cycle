60d7bb40c6482e5e5c9a2b56765dc24264684bcc
Liam Mills <liam.mills1511@gmail.com>
2023-08-26 13:41:00 +1000

    css classes setup

--- frontend/quasar.config.js
+++ frontend/quasar.config.js
@@ -30,7 +30,7 @@ module.exports = configure(function (/* ctx */) {
     boot: ["axios"],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
-    css: ["app.scss"],
+    css: ["app.sass"],
 
     // https://github.com/quasarframework/quasar/tree/dev/extras
     extras: [
