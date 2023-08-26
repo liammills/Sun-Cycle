ddb993ed0f55001aaec57727b44582c098218cc5
Bangshuo <bang@hospopay.com>
2023-08-26 14:53:49 +1000

    footer done

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -7,21 +7,7 @@
       <RouterView />
     </QPageContainer>
 
-    <QFooter class="bg-grey-9 text-white q-px-md q-py-lg">
-      <QToolbar>
-        <QToolbarTitle>
-          <img
-            class="q-mb-sm"
-            style="width: 120px"
-            src="src/assets/suncycle-logo-white.svg"
-          >
-        </QToolbarTitle>
-        <div class="row q-gutter-md">
-          <span>About Us</span>
-          <span>Marketplace</span>
-        </div>
-      </QToolbar>
-    </QFooter>
+    <SCFooter />
 
   </QLayout>
 </template>
@@ -29,9 +15,10 @@
 <script>
 import { defineComponent } from 'vue'
 import SCHeader from 'src/components/SCHeader.vue';
+import SCFooter from 'src/components/SCFooter.vue';
 
 export default defineComponent({
     name: 'MainLayout',
-    components: { SCHeader }
+    components: { SCHeader, SCFooter, }
 })
 </script>
