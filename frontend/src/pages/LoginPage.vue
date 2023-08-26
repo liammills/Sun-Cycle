7fafd7ae6f029ff70a8b7f2adaec21e71c2d1512
Liam Mills <liam.mills1511@gmail.com>
2023-08-26 13:22:15 +1000

    first component added - wip

---
+++ frontend/src/pages/LoginPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex flex-center">
+    <LoginForm />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import LoginForm from '../components/LoginForm.vue';
+
+export default defineComponent({
+  name: 'LoginPage',
+  components: {
+    LoginForm,
+  },
+})
+</script>
