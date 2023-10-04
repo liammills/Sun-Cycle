f0a4f9bc6c6f88e9cccc540c6c0622c867b65339
Liam Mills <liam.mills1511@gmail.com>
2023-10-04 19:58:56 +1100

    marketplace page fixed up

--- frontend/src/pages/MarketplacePage.vue
+++ frontend/src/pages/MarketplacePage.vue
@@ -1,95 +1,51 @@
 <template>
   <q-page class="flex justify-center">
-    <div class="row main-container large-container justify-between">
-      <div class="column">
-        <h1>Discover installations</h1>
-        <div class="row q-my-xl">
-          <div class="column q-mr-md">
-            <QSelect class="q-mb-md" outlined v-model="selectedRecyclingMethod" :options="recyclingMethodOptions" label="Method of recycling" style="width: 300px;" />
-            <QInput outlined v-model="text" label="City/town" />
-          </div>
-          <div class="column">
-            <QInput class="q-mb-md" outlined v-model="text" type="date" label="Retired by" />
-            <QSelect outlined v-model="selectedState" :options="stateOptions" label="State" />
-          </div>
+    <div class="main-container large-container">
+      <h1>Discover installations</h1>
+      <div class="row q-mt-lg">
+        <div class="column q-mr-md">
+          <QSelect class="q-mb-md" outlined v-model="selectedRecyclingMethod" :options="recyclingMethodOptions" label="Method of recycling" style="width: 300px;" />
+          <QInput outlined v-model="text" label="City/town" />
         </div>
-        <h3 class="q-mb-md">Material breakdown</h3>
-        <div class="row q-mb-md">
-          <div class="column q-mr-xl">
-            <div class="row justify-between items-center matbreakpair">
-              <span>Silicone</span>
-              <QInput outlined v-model.number="siliconeinput" type="number" class="matbreakinput">
-                <template v-slot:append>
-                  <span style="font-size: 14px;">g</span>
-                </template>
-              </QInput>
-            </div>
-          </div>
-          <div class="column">
-            <div class="row justify-between items-center matbreakpair">
-              <span>Silver</span>
-              <QInput outlined v-model.number="siliconeinput" type="number" class="matbreakinput">
-                <template v-slot:append>
-                  <span style="font-size: 14px;">g</span>
-                </template>
-              </QInput>
-            </div>
-          </div>
+        <div class="column">
+          <QInput class="q-mb-md" outlined v-model="text" type="date" label="Retired by" />
+          <QSelect outlined v-model="selectedState" :options="stateOptions" label="State" />
         </div>
-        <div class="row q-mb-md">
-          <div class="column q-mr-xl">
-            <div class="row justify-between items-center matbreakpair">
-              <span>Polymers</span>
-              <QInput outlined v-model.number="siliconeinput" type="number" class="matbreakinput">
-                <template v-slot:append>
-                  <span style="font-size: 14px;">g</span>
-                </template>
-              </QInput>
-            </div>
-          </div>
-          <div class="column">
-            <div class="row justify-between items-center matbreakpair">
-              <span>Aluminium</span>
-              <QInput outlined v-model.number="siliconeinput" type="number" class="matbreakinput">
-                <template v-slot:append>
-                  <span style="font-size: 14px;">g</span>
-                </template>
-              </QInput>
-            </div>
-          </div>
-        </div>
-        <div class="row q-mb-xl">
-          <div class="column q-mr-xl">
-            <div class="row justify-between items-center matbreakpair">
-              <span>Copper</span>
-              <QInput outlined v-model.number="siliconeinput" type="number" class="matbreakinput">
-                <template v-slot:append>
-                  <span style="font-size: 14px;">g</span>
-                </template>
-              </QInput>
-            </div>
-          </div>
-          <div class="column">
-            <div class="row justify-between items-center matbreakpair">
-              <span>Glass</span>
-              <QInput outlined v-model.number="siliconeinput" type="number" class="matbreakinput">
-                <template v-slot:append>
-                  <span style="font-size: 14px;">g</span>
-                </template>
-              </QInput>
+        <div
+          class="q-ml-xl"
+          style="max-width: 500px; margin-top: -42px"
+        >
+          <h3 class="q-pb-md">Material breakdown</h3>
+          <div class="q-gutter-lg row q-col-gutter-xs">
+            <div class="q-col-4" v-for="material in materials" :key="material.id">
+              <div
+                class="row justify-between items-center"
+                style="width: 190px"
+              >
+                <span class="q-mr-md">{{ material.name }}</span>
+                <QInput 
+                  outlined
+                  dense
+                  v-model.number="material.input" 
+                  type="number" 
+                  style="width: 100px;"
+                >
+                  <template v-slot:append>
+                    <span style="font-size: 14px;">g</span>
+                  </template>
+                </QInput>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <div class="q-ml-xl column self-end">
-        <div class="full-width full-height">
-          <GMapMap
-            :center="center"
-            :zoom="7"
-            style="width: 500px; height: 500px"
-          >
-          </GMapMap>
-        </div>
+      <div class="q-my-xl">
+        <GMapMap
+          :center="center"
+          :zoom="7"
+          style="width: 1100px; height: 600px"
+        >
+        </GMapMap>
       </div>
     </div>
   </q-page>
@@ -102,13 +58,18 @@ export default {
     return {
       stateOptions: ["NSW", "QLD", "SA", "TAS", "VIC", "WA" ],
       selectedState: '',
-
       recyclingMethodOptions: ["Chemical processing", "Electrochemical processing", "Hydrometallurgical separation", "Mechanical processing", "Thermal processing"],
       selectedRecyclingMethod: '',
-
-      siliconeinput: '',
+      materials: [
+        { id: 1, name: 'Silicone', input: '' },
+        { id: 2, name: 'Silver', input: '' },
+        { id: 3, name: 'Polymers', input: '' },
+        { id: 4, name: 'Aluminium', input: '' },
+        { id: 5, name: 'Copper', input: '' },
+        { id: 6, name: 'Glass', input: '' }
+      ],
       center: {lat: 51.093048, lng: 6.842120},
-
+      radius: 10000,
       markers: [],
     };
   },
@@ -134,7 +95,7 @@ export default {
             params: {
               latitude: this.center.lat,
               longitude: this.center.lng,
-              radius: 100000,
+              radius: this.radius,
               state: this.selectedState,
               recycling_method: this.selectedRecyclingMethod,
             },
@@ -158,13 +119,3 @@ export default {
   }
 }
 </script>
-
-<style>
-.matbreakpair {
-  width: 200px;
-}
-
-.matbreakinput {
-  width: 100px;
-}
-</style>
